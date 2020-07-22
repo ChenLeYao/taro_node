c37a2c688aab84e5e56b21f322d72398eda930ac
cqq <18814515448@163.com>
2020-07-22 15:01:33 +0800

    'change_pool'

--- taro_node/mysql/mysql.js
+++ taro_node/mysql/mysql.js
@@ -1,57 +1,74 @@
 ﻿var mysql = require('mysql');
 //连接数据库
-
-var db = mysql.createConnection({
+var db = {
     host : 'localhost',
-
     user : 'root',
     // localAddress : '127.0.0.8' ,
     port: 3306,
     password : '776824324' ,
     database : 'taro' ,
+    acquireTimeout: 10000 ,
+    waitForConnctions:true,
+    queueLimit : 0,
     multipleStatements : true //允许执行多条语句
-});
-
-
-function select ( ql , callback ){
-    ql = ql || 'select * from card_list';
-    db.connect(()=>{
-        console.log('数据库连接成功');
-    });
-     new Promise(function( rel , rej){
-
-        db.query( ql , function ( err , data ) {
-        console.log(err );
-        console.log(data );
-        callback && callback( err , data);
-        rel();
-     });
-     }).then(()=>{
-        // closeMysql(db);
-     })
-    
-
 };
+var pool = mysql.createPool(db);
 
-
-//查询成功后关闭mysql
-
-function closeMysql(connect){
-    
-   connect.end((err)=>{
-     
-   if(err){
-          
-        console.log(`mysql关闭失败:${err}!`);
-   
-    }else{
-          
-        console.log('mysql关闭成功!');
-  
-    }
-});
-
+function select(  ql , callback  ) {
+    ql = ql || 'select * from card_list';
+    pool.getConnection(( err , connection )=>
+        {
+            new Promise(function( rel , rej){
+                connection.query( ql , function ( err , data ) {
+                    callback && callback( err , data);
+                    rel();
+                });
+            }).then(()=>{
+                // closeMysql(db);
+            })
+        }
+    )
 }
-
-
-module.exports = { select : select  , db : db  ,closeMysql : closeMysql };
+// var db = mysql.createConnection({
+//     host : 'localhost',
+//     user : 'root',
+//     // localAddress : '127.0.0.8' ,
+//     port: 3306,
+//     password : '776824324' ,
+//     database : 'taro' ,
+//     multipleStatements : true //允许执行多条语句
+// });
+// function select ( ql , callback ){
+//     ql = ql || 'select * from card_list';
+//     db.connect(()=>{
+//         console.log('数据库连接成功');
+//     });
+//      new Promise(function( rel , rej){
+//         db.query( ql , function ( err , data ) {
+//         console.log(err );
+//         console.log(data );
+//         callback && callback( err , data);
+//         rel();
+//      });
+//      }).then(()=>{
+//         // closeMysql(db);
+//      })
+// };
+// //查询成功后关闭mysql
+// function closeMysql(connect){
+//
+//    connect.end((err)=>{
+//
+//    if(err){
+//
+//         console.log(`mysql关闭失败:${err}!`);
+//
+//     }else{
+//
+//         console.log('mysql关闭成功!');
+//
+//     }
+// });
+//
+// }
+module.exports = { select : select  , db : db  };
